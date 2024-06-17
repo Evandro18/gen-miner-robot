266c0cdc0307d871f2b230994d2fb93d8fc61a23
Evandro Terra Gonçalves <960738@lojasrenner.com.br>
2024-06-16 20:27:14 -0400

    feat(extractor): code organization and write in csv file

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import asyncio
 from src.infra.commads.scheduled_data_extraction import ScheduledDataExtraction
-from src.infra.repositories.get_auction_baches_repository import search_auction_batches
+from src.infra.repositories.get_auction_baches_repository import PypeteerExtractor
 from src.use_cases.get_auction_batches_usecase import GetAuctionBatchesUseCase
 
 
 async def main():
-    scheduled_data_extraction = ScheduledDataExtraction(GetAuctionBatchesUseCase(auction_repo=search_auction_batches))
+    url = 'https://vitrinedejoias.caixa.gov.br/Paginas/Busca.aspx'
+    extractor = PypeteerExtractor(url)
+    scheduled_data_extraction = ScheduledDataExtraction(GetAuctionBatchesUseCase(auction_repo=extractor))
     await scheduled_data_extraction.run()
     
 if __name__ == '__main__':
--- src/use_cases/get_auction_batches_usecase.py
+++ src/use_cases/get_auction_batches_usecase.py
@@ -1,21 +1,39 @@
-from collections.abc import AsyncIterable
-from typing import Callable
+from typing import AsyncIterable, Protocol
 
+
+class SearchAuctionBatchesRepository(Protocol):
+    def execute(self) -> AsyncIterable[dict[str, str]]:
+        raise NotImplementedError
 
 class GetAuctionBatchesUseCase:
-    def __init__(self, auction_repo: Callable[[], AsyncIterable[dict[str, str]]]):
+    def __init__(self, auction_repo: SearchAuctionBatchesRepository):
         self.get_auction_data = auction_repo
 
     async def execute(self) -> bool:
         try:
-            items = []
-            iterator = self.get_auction_data()
-            async for batch in iterator:
-                items.append(batch)
-                if len(items) == 10:
-                    # save items
-                    items = []
+            # items = []
+            counter = 0
+            iterator = self.get_auction_data.execute()
+            with open("auctions.csv", "w", encoding='utf-8') as file:
+                async for batch in iterator:
+                    if counter == 0:
+                        batch_keys = batch.keys()
+                        file.write("; ".join(batch_keys) + "\n")
+                        counter += 1
+                    
+                    values = [self._value_to_string(v) for v in batch.values()]
+                    csv_row = ("; ".join(values) + '\n')
+                    file.write(csv_row)
+                    
             return True
         except Exception as e:
             return False
+    
+    def _value_to_string(self, value):
+        if isinstance(value, list):
+            return ", ".join(value)
+
+        if ';' in str(value):
+            value = str(value).replace(';', '.')
+        return str(value)
         
